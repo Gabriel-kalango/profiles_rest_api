20abdc5aaa492ae4905437f7c102732ff6d9441b
Gabriel Kalango <kallythegreat11@gmail.com>
2024-07-02 04:50:02 -1100

    added hello api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """test Api view """
+    
+    def get(self,request,format=None):
+        """returns a list of apiview features"""
+        an_apiview=[ 'uses http methods as function (get,post,patch,put,delete)',
+                    'is simillar to a traditional django view',
+                    'kally the great!!!']
+        
+        return Response({'message':"hello",
+                         "an_apiview":an_apiview})
